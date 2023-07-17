e595a9088ad7baa0a694a9a4fb6e091ed8dce5e4
Sergio Miramon <smiramon8@gmail.com>
2023-07-17 10:25:15 +0200

    refactor project

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -2,10 +2,10 @@ import "./Header.css";
 
 const template = () => {
     return `
-    <a href="./index.html"><img src="./assets/images/all/logo.png" class="logo" alt="logo"></a>
+    <!-- <a href="./index.html"><img src="/assets/images/all/logo.png" class="logo" alt="logo"></a> -->
         <nav>
             <ul class="enlaces">
-                <!-- <li><a href=""><i class="arrows icon-arrow-left2"></i></a></li> -->
+                <li><a href="https://github.com/SergioMiramon/photo-gallery" target="blank"><img src="/assets/images/all/github.png" class="" alt="github"></a></li>
             </ul>
         </nav>
     `;
